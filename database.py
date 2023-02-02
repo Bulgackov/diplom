cdef8e93ab85090c43015d2507a24001fbc47625
Bulgackov <84766982+Bulgackov@users.noreply.github.com>
2023-02-02 06:34:13 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,98 +1,55 @@
-import psycopg2
-from config import *
+from sqlalchemy import Integer, String, Date, Boolean, Column, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
-connection = psycopg2.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=db_name
-)
+from connect import BDConnect
 
-connection.autocommit = True
+Base = declarative_base()
+engine = BDConnect.engine
+
+session = BDConnect.session
 
 
-def create_table_users():
-    """СОЗДАНИЕ ТАБЛИЦЫ USERS (НАЙДЕННЫЕ ПОЛЬЗОВАТЕЛИ"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS users(
-                id serial,
-                first_name varchar(50) NOT NULL,
-                last_name varchar(25) NOT NULL,
-                vk_id varchar(20) NOT NULL PRIMARY KEY,
-                vk_link varchar(50));"""
-        )
-    print("[INFO] Table USERS was created.")
+class VKinderUser(Base):
+    __tablename__ = 'vkinder_user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    vk_id = Column(Integer)
+    first_name = Column(String)
+    last_name = Column(String)
+    date_of_birth = Column(String)
+    city_id = Column(Integer)
+    city_title = Column(String)
 
 
-def create_table_seen_users():  # references users(vk_id)
-    """СОЗДАНИЕ ТАБЛИЦЫ SEEN_USERS (ПРОСМОТРЕННЫЕ ПОЛЬЗОВАТЕЛИ"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS seen_users(
-            id serial,
-            vk_id varchar(50) PRIMARY KEY);"""
-        )
-    print("[INFO] Table SEEN_USERS was created.")
+class SearchParams(Base):
+    __tablename__ = 'search_params'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_User = Column(Integer, ForeignKey('vkinder_user.id'))
+    gender = Column(Integer, nullable=False)
+    age_from = Column(Integer, nullable=False)
+    age_to = Column(Integer, nullable=False)
+    date = Column(Date)
 
 
-def insert_data_users(first_name, last_name, vk_id, vk_link):
-    """ВСТАВКА ДАННЫХ В ТАБЛИЦУ USERS"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            f"""INSERT INTO users (first_name, last_name, vk_id, vk_link) 
-            VALUES ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}');"""
-        )
+class SearchResult(Base):
+    __tablename__ = 'search_result'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    vk_id = Column(Integer, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    date_of_birth = Column(String, default='')
+    city_id = Column(Integer)
+    city_title = Column(String)
+    id_User = Column(Integer, ForeignKey('vkinder_user.id'))
+    search_date = Column(Date)
+    viewed = Column(Boolean, default=False)
 
 
-def insert_data_seen_users(vk_id, offset):
-    """ВСТАВКА ДАННЫХ В ТАБЛИЦУ SEEN_USERS"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            f"""INSERT INTO seen_users (vk_id) 
-            VALUES ('{vk_id}')
-            OFFSET '{offset}';"""
-        )
-
-
-def select(offset):
-    """ВЫБОРКА ИЗ НЕПРОСМОТРЕННЫХ ЛЮДЕЙ"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            f"""SELECT u.first_name,
-                        u.last_name,
-                        u.vk_id,
-                        u.vk_link,
-                        su.vk_id
-                        FROM users AS u
-                        LEFT JOIN seen_users AS su 
-                        ON u.vk_id = su.vk_id
-                        WHERE su.vk_id IS NULL
-                        OFFSET '{offset}';"""
-        )
-        return cursor.fetchone()
-
-
-def drop_users():
-    """УДАЛЕНИЕ ТАБЛИЦЫ USERS КАСКАДОМ"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE IF EXISTS users CASCADE;"""
-        )
-        print('[INFO] Table USERS was deleted.')
-
-
-def drop_seen_users():
-    """УДАЛЕНИЕ ТАБЛИЦЫ SEEN_USERS КАСКАДОМ"""
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE  IF EXISTS seen_users CASCADE;"""
-        )
-        print('[INFO] Table SEEN_USERS was deleted.')
-
-
-def creating_database():
-    drop_users()
-    drop_seen_users()
-    create_table_users()
-    create_table_seen_users()
+class ViewedUsers(Base):
+    __tablename__ = 'viewed_users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_SearchResult = Column(Integer, ForeignKey('search_result.id'))
+    status = Column(Integer)
+    
+    
+def create_tables():
+Base.metadata.create_all(engine)
